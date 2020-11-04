baffc582efc0e98c8984fad81b260a16a61612a1
Jennifer Combs <jco-solutions@outlook.com>
2020-11-04 12:19:24 -0500

    first commit for actor exercise

---
+++ script.js
@@ -0,0 +1,35 @@
+var actor1 = {
+    firstName: "Tom",
+    lastName: "Cruise",
+    age: 59,
+    yearlyEarnings: 1000000,
+}
+
+var actor2 = {
+    firstName: "Chandler",
+    lastName: "Riggs",
+    age: 20,
+    yearlyEarnings: 500000,
+}
+
+var actor3 = {
+    firstName: "Emma",
+    lastName: "Roberts",
+    age: 29,
+    yearlyEarnings: 600000,
+}
+
+var actor4 = {
+    firstName: "Helen",
+    lastName: "Mirren",
+    age: 75,
+    yearlyEarnings: 999000,
+}
+
+
+
+function actorAge(actorArray) {
+for (let i = 0; i < actoryArray.length; i++) {
+console.log(actorArray[i]);
+    }
+}
